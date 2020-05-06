0534e2686a2b0dded9dc531d978df051467e9f76
David Rheinsberg <david.rheinsberg@gmail.com>
2020-05-06 16:03:38 +0200

    ghci: import GitHub CI modules
    This imports the modules currently used for the GitHub Actions based
    CI. The idea is to consolidate them in a central place so we do not
    have to ship them in each upstream repository.

---
+++ ghci/actions/ghci-osbuild/index.js
@@ -0,0 +1,70 @@
+const core = require('@actions/core');
+const proc = require('child_process');
+
+try {
+        const arg_actor = String(core.getInput('actor'));
+        const arg_image = String(core.getInput('image'));
+        const arg_run = String(core.getInput('run'));
+        const arg_token = String(core.getInput('token'));
+
+        arg_cwd = process.cwd()
+
+        if (arg_token.length > 0) {
+                console.log('Authenticate to GitHub Packages');
+                console.log('--------------------------------------------------------------------------------');
+                proc.execFileSync(
+                        '/usr/bin/docker',
+                        [
+                                'login',
+                                'docker.pkg.github.com',
+                                '--username',
+                                arg_actor,
+                                '--password',
+                                arg_token
+                        ],
+                        {
+                                stdio: 'inherit'
+                        }
+                );
+                console.log('--------------------------------------------------------------------------------');
+        }
+
+        console.log('Pull CI Image');
+        console.log('--------------------------------------------------------------------------------');
+        proc.execFileSync(
+                '/usr/bin/docker',
+                [
+                        'pull',
+                        '--quiet',
+                        arg_image
+                ],
+                {
+                        stdio: 'inherit'
+                }
+        );
+        console.log('--------------------------------------------------------------------------------');
+
+        console.log('Execute CI');
+        console.log('--------------------------------------------------------------------------------');
+        proc.execFileSync(
+                '/usr/bin/docker',
+                [
+                        'run',
+                                '--privileged',
+                                '--rm',
+                                '--volume=' + arg_cwd + ':/ci/workdir',
+                                arg_image,
+                                '/bin/bash',
+                                        '-c',
+                                        arg_run
+                ],
+                {
+                        stdio: 'inherit'
+                }
+        );
+        console.log('--------------------------------------------------------------------------------');
+
+        console.log(`End of CI`);
+} catch (error) {
+        core.setFailed(error.message);
+}
