014064bcfa182eccec8e435293be3d99a8501f56
Ondřej Budai <obudai@redhat.com>
2020-08-24 15:00:46 +0200

    containers/koji: add Kerberos/GSSAPI auth support
    Previously, users were only able to authenticate using username & password.
    This does not reflect the production instances of koji though because as far
    as I know, none of them supports this particular authentication type.
    
    This commit adds support for Kerberos/GSSAPI/SPNEGO authentication type. Note
    that you need a working KDC for this setup to work. See the README in
    Dockerfile.

--- src/containers/ghci-koji/ci/run-koji.sh
+++ src/containers/ghci-koji/ci/run-koji.sh
@@ -6,9 +6,33 @@ sed -i  -e "s/.*DBHost =.*/DBHost = ${POSTGRES_HOST}/" \
         -e "s/.*DBUser =.*/DBUser = ${POSTGRES_USER}/" \
         -e "s/.*DBPass =.*/DBPass = ${POSTGRES_PASSWORD}/" \
         -e "s/.*DBName =.*/DBName = ${POSTGRES_DB}/" \
+        -e "s|.*AuthPrincipal =.*|AuthPrincipal = host/kojihub@LOCAL|" \
+        -e "s|.*AuthKeytab =.*|AuthKeytab = /share/koji.keytab|" \
+        -e "s|.*KojiDebug =.*|KojiDebug = On|" \
+        -e "s|.*LogLevel =.*|LogLevel = DEBUG|" \
         /etc/koji-hub/hub.conf
 
 sed -i -e "s|LogLevel warn|LogLevel debug|" /etc/httpd/conf/httpd.conf
+
+tee -a /etc/httpd/conf.d/kojihub.conf <<END
+<Location /kojihub/ssllogin>
+        AuthType GSSAPI
+        GssapiSSLonly Off
+        GssapiLocalName Off
+        AuthName "GSSAPI Single Sign On Login"
+        GssapiCredStore keytab:/share/koji.keytab
+        Require valid-user
+</Location>
+END
+
+sed -i  -e "s|^SSLCertificateFile.*|SSLCertificateFile /share/crt.pem|" \
+        -e "s|^SSLCertificateKeyFile.*|SSLCertificateKeyFile /share/key.pem|" \
+        -e "s|LogLevel warn|LogLevel debug|" \
+        -e "s|^#ServerName.*|ServerName localhost|" \
+        /etc/httpd/conf.d/ssl.conf
+
+sed -i  -e "s|^#ServerName.*|ServerName localhost|" \
+        /etc/httpd/conf/httpd.conf
 
 # wait for postgres to come on-line
 timeout 10 bash -c "until printf '' 2>/dev/null >/dev/tcp/${POSTGRES_HOST}/5432; do sleep 0.1; done"
