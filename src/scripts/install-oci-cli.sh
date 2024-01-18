2a325d291e17d557e51d056ff0a969fb13418e34
Tomáš Hozza <thozza@redhat.com>
2024-01-18 14:05:00 +0100

    install-oci-cli.sh: workaround issues with Python 3.12
    Workaround https://github.com/oracle/oci-cli/issues/742
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- src/scripts/install-oci-cli.sh
+++ src/scripts/install-oci-cli.sh
@@ -2,4 +2,18 @@
 
 set -eox pipefail
 
-bash -c "$(curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh)" -- --accept-all-defaults
+# temporary filename for the script
+INSTALL_SCRIPT_PATH="mktemp"
+
+curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh --output "${INSTALL_SCRIPT_PATH}"
+
+# install python3.11 if the python3 version is 3.12 or newer
+# https://github.com/oracle/oci-cli/issues/742
+if [[ $(python3 -c "import sys; too_new = sys.version_info >= (3, 12, 0); print(1 if too_new else 0)") -eq 1 ]]; then
+    sudo dnf install -y python3.11
+    sudo dnf clean all
+    sed -i 's/for try_python_exe in /&python3.11 /' "${INSTALL_SCRIPT_PATH}"
+fi
+
+
+bash "${INSTALL_SCRIPT_PATH}" --accept-all-defaults
