750f81ca19fa15fff9ed497e909bc8d093be4373
Tomáš Hozza <thozza@redhat.com>
2025-01-16 14:19:11 +0100

    scripts/install-oci-cli: don't use sudo to run DNF
    For unknown reason, the script started to fail with:
    ```
    sudo: PAM account management error: Authentication service cannot retrieve authentication info
    sudo: a password is required
    ```
    
    Since the other script that runs DNF does not use sudo, let's not use it
    in this one either.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- src/scripts/install-oci-cli.sh
+++ src/scripts/install-oci-cli.sh
@@ -10,8 +10,8 @@ curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/
 # install python3.11 if the python3 version is 3.12 or newer
 # https://github.com/oracle/oci-cli/issues/742
 if [[ $(python3 -c "import sys; too_new = sys.version_info >= (3, 12, 0); print(1 if too_new else 0)") -eq 1 ]]; then
-    sudo dnf install -y python3.11
-    sudo dnf clean all
+    dnf install -y python3.11
+    dnf clean all
     sed -i 's/for try_python_exe in /&python3.11 /' "${INSTALL_SCRIPT_PATH}"
 fi
 
